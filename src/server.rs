e6d8c9293a8d4f8251c07a9099baa4a577470f20
hryhorii <grimy.grigory@gmail.com>
2024-06-01 02:52:04 +0200

    returned to simple messages

--- src/server.rs
+++ src/server.rs
@@ -1,103 +1,53 @@
-use std::{collections::HashMap, net::SocketAddr, sync::Arc};
-use serde_json::from_str;
-use tokio::{net::TcpListener, sync::{mpsc, RwLock}};
-use tokio_tungstenite::{accept_async, tungstenite::{self, Error as Err}};
-use tungstenite::Result as Res;
-use futures_util::{StreamExt, SinkExt};
-use uuid::Uuid;
+use tokio::{io::{AsyncBufReadExt, AsyncWriteExt, BufReader}, net::TcpListener, sync::broadcast};
 
 use crate::{message::Message, utils::current_timestamp};
 
-type Users = Arc<RwLock<HashMap<String, mpsc::UnboundedSender<tungstenite::Message>>>>;
 
-async fn accept_connection(peer: SocketAddr, stream: tokio::net::TcpStream, users: Users) {
-    if let Err(e) = handle_connection(peer, stream, users).await {
-        match e {
-            Err::ConnectionClosed | Err::Protocol(_) | Err::Utf8 => (),
-            err => eprintln!("Error processing connection: {:?}", err),
-        }
-    }
-}
-
-async fn handle_connection(peer: SocketAddr, stream: tokio::net::TcpStream, users: Users) -> Res<()> {
-    let ws_stream = accept_async(stream).await.expect("Failed to accept");
-
-    println!("New WebSocket connection: {}", peer);
-
-    let (tx, mut rx) = mpsc::unbounded_channel();
-
-    
-
-    let (mut write, mut read) = ws_stream.split();
-
-    let user_id = Uuid::new_v4().to_string();
-    users.write().await.insert(user_id.clone(), tx);
-
-    let user_connected_msg = Message::UserConnected {
-        sender: "server".to_string(),
-        content: format!("User {} has connected.", user_id),
-        timestamp: current_timestamp(),
-        client_id: user_id
-    };
-
-    broadcast_message(&users, &user_connected_msg).await;
-
-    tokio::spawn(async move {
-        while let Some(msg) = rx.recv().await {
-            if write.send(msg).await.is_err() {
-                break;
-            }
-        }
-    });
-
-    while let Some(message) = read.next().await {
-        match message {
-            Ok(msg) => {
-                if msg.is_text() {
-                    let text = msg.to_text().unwrap();
-                    if let Ok(chat_msg) = from_str::<Message>(text) {
-                        match chat_msg {
-                            Message::Chat { recipient, .. } => {
-                                println!("Received message: {}", text);
-                                if let Some(tx) = users.read().await.get(&recipient) {
-                                    let _ = tx.send(tungstenite::Message::Text(text.to_string()));
-                                    println!("Sent message: {}", text);
-                                }
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-            }
-            Err(e) => {
-                eprintln!("WebSocket error: {}", e);
-                break;
-            }
-        }
-    }
-    Ok(())
-}
-
-async fn broadcast_message(users: &Users, message: &Message) {
-    let msg_text = serde_json::to_string(&message).unwrap();
-    for user in users.read().await.values() {
-        let _ = user.send(tungstenite::Message::Text(msg_text.clone()));
-    }
-}
 
 #[tokio::main]
 pub async fn run(){
     let addr = "127.0.0.1:3000";
     let listener = TcpListener::bind(&addr).await.expect("Failed to bind");
 
-    println!("Listening on: {}", addr);
+    println!("server is up on: {}", addr);
 
-    let users: Users = Arc::new(RwLock::new(HashMap::new()));
+    let (tx, _rx) = broadcast::channel(10);
 
-    while let Ok((stream, _)) = listener.accept().await {
-        let peer = stream.peer_addr().expect("connected streams should have a peer address");
-        let users = users.clone();
-        tokio::spawn(accept_connection(peer, stream, users));
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+
+        println!("New Server connection: {}", addr);
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 { // TODO change logic 
+                            break;
+                        }
+
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+
+                        if addr != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap()
+                        }
+                    }
+                }
+            }
+
+        });
     }
 }
 
